1c0489d92d62f7744d556c3566e7f56211f818f8
Rangel2609 <69400987+Rangel2609@users.noreply.github.com>
2020-08-24 21:33:07 -0600

    Practica

---
+++ PracticaUnica/Practica.py
@@ -0,0 +1,18 @@
+print("Bienvenido")
+input("Presiona Enter para Comenzar")
+def opciones():
+    print("0")
+print("1. Cargar")    
+print("2. Seleccionar")
+print("3. Maximo")
+print("4. Minimo")
+print("5. Suma")
+print("6. Cuenta")
+print("7. Reportar")
+
+
+
+
+
+
+
