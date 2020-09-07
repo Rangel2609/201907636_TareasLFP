27fd29841e4ee9fee8b1cd06a5a15b15c83f3c29
Rangel2609 <69400987+Rangel2609@users.noreply.github.com>
2020-09-06 20:40:01 -0600

    8:39pm

--- Tarea 5/AFD.py
+++ Tarea 5/AFD.py
@@ -1,32 +1,27 @@
 def AFD(ingreso):
     estado = 0;
-    ellipsis = ''
+    epsilon = ''
     for compara in range(len(ingreso)):
         if estado == 0:
-            try:
-                int(ingreso)
-                return False
-            except(True, ValueError):
-                return True
+            if ingreso[compara] == epsilon:
+                estado == 1;
+                if ingreso[compara] == ['']:
+                    estado == 2;
+                    if ingreso[compara] == [int()]:
+                        estado ==3;
 
-        elif ingreso[compara] == ellipsis:
-                 estado ==0;
+        else: estado == [''];
+        if estado ==0:
+            if ingreso[compara] == ['']:
+                estado == 1;
+                if ingreso[compara] == ['']:
+                    estado == 2;
+                    if ingreso[compara] == [int()]:
+                        estado ==3;
+        break
+        print("La cadena ingresada es correcta")
 
-        else: print("Entrada de cadena incorrecta")
 
-        if ingreso[compara] == 1:
 
-            ingreso[compara] = 1;
-            estado == 1;
 
-        elif ingreso[compara] == 3:
-            ingreso[compara] == '';
-            estado ==2;
 
-        elif ingreso[compara] == 4:
-            ingreso[compara] == '';
-            estado == 3;
-
-        elif ingreso[compara] == 5:
-            ingreso[compara] == 0;
-            estado ==4;
